fdcc085637a406271b5c7d8fef0547cf81ae9fd9
Ravi Singh <ravi98397@gmail.com>
2024-08-23 19:17:51 +0530

    initial git config

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+
+local  builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+
